55b8da3a294cfb151481b62b3d76d5cf535e26be
ZetsubouTensai <nguyenvantothanh@gmail.com>
2021-07-20 21:01:34 +0700

    Update cau 3

---
+++ src/components/Home/Home.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import './Home.css';
+
+class Home extends Component {
+  render() {
+    return <div>This is home Page</div>;
+  }
+}
+
+export default Home;
---
+++ src/components/Home/index.js
@@ -0,0 +1,6 @@
+export { default } from "./Home";
+import React from "react";
+import ReactDOM from "react-dom";
+import Home from "./Home/Home.js";
+
+ReactDOM.render(<Home />, document.getElementById("root"));
